d388971e2343fa7aacdac06f93a607a871d3f885
thecsw <sagindyk.urazayev@protonmail.ch>
2017-11-14 17:50:28 +0400

    Added appropriateconfig file

---
+++ example.config.py
@@ -0,0 +1,8 @@
+#This is for reddit
+client_id = 'take it from your account\'s preferences'
+client_secret = 'take it from your account\'s preferences'
+username = 'username'
+password = 'password'
+user_agent = 'something'
+#This is for telegram
+token = 'YOUR TELEGRAM API KEY'

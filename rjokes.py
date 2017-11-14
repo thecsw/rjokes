686ccd2fc31cb56e2890b8b4c682fc8cdfa548c5
thecsw <sagindyk.urazayev@protonmail.ch>
2017-11-14 18:19:28 +0400

    Source code

--- rjokes.py
+++ rjokes.py
@@ -1,8 +1,44 @@
+import config
 import telepot
+from telepot.loop import MessageLoop
 import praw
+import random
+import time
 
-reddit = praw.Reddit(client_id =
-                     client_secret =
-                     username = ''
-                     password = ''
-                     user_agent = '')
+LIMIT = 200
+jokesTitles = []
+jokesTexts = []
+reddit = praw.Reddit(client_id = config.client_id,
+                     client_secret = config.client_secret,
+                     username = config.username,
+                     password = config.password,
+                     user_agent = config.user_agent)
+
+telebot = telepot.Bot(config.token)
+
+subreddit = reddit.subreddit('Jokes')
+
+hot_python = subreddit.hot(limit=LIMIT)
+
+def handle(msg):
+    user_id = msg['chat']['id']
+    command = msg['text']
+    if command == '/joke':
+        joke = random.randint(1, LIMIT-1)
+        telebot.sendChatAction(user_id, 'typing')
+        telebot.sendMessage(user_id, jokesTitles[joke])
+        time.sleep(1)
+        telebot.sendChatAction(user_id, 'typing')
+        telebot.sendMessage(user_id, jokesTexts[joke])
+        print(user_id)
+
+MessageLoop(telebot, handle).run_as_thread()
+while (1):
+    jokesTitles = []
+    jokesTexts = []
+    for submission in hot_python:
+        if not submission.stickied:
+            jokesTitles.append(submission.title)
+            jokesTexts.append(submission.selftext)
+    print('Updated list!')
+    time.sleep(100)
